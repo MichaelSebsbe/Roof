77eba25dc89dd15982ef34cdfdb4f462bbd43975
Michael Sebsbe <Michael.mulugeta2010@gmail.com>
2022-06-24 11:43:41 -0400

    Initial Commit

---
+++ Roof/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Roof
+//
+//  Created by Michael Sebsbe on 6/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Roof/RoofApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoofApp.swift
+//  Roof
+//
+//  Created by Michael Sebsbe on 6/24/22.
+//
+
+import SwiftUI
+
+@main
+struct RoofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
